7e2baae2053528f013e8582c80413b7d357d428c
Matheus Murbach <matheus.murbach310702@gmail.com>
2023-03-03 11:11:23 -0300

    Reorganização

--- Python/Calculadora.py
+++ Python/Calculadora.py
@@ -1,17 +1,19 @@
 import sys
 import os
 
-diretório=os.getcwd()
+diretório = os.getcwd()
 print('Lista de argumentos: ', str(sys.argv))
 print('total de argumentos: ', len(sys.argv), ' argumentos')
+
 
 def addition():
     print("O argumento [0] é :", sys.argv[0])
     print("O argumento [1] é :", sys.argv[1])
     print("O argumento [2] é :", sys.argv[2])
-    num1=sys.argv[1]
-    num2=sys.argv[2]
-    soma=int(num1)+int(num2)
+    num1 = sys.argv[1]
+    num2 = sys.argv[2]
+    soma = int(num1)+int(num2)
     print("A soma de {0} e {1} é {2}".format(num1, num2, soma))
+
 
 addition()
