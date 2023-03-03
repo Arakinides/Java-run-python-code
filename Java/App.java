76ae198215f419a41ba86b7ad83d2d1a31bbd699
Matheus Murbach <matheus.murbach310702@gmail.com>
2023-03-03 11:07:26 -0300

    Reorganização

--- Java/App.java
+++ Java/App.java
@@ -1,41 +1,38 @@
 package Java;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 class App {
       public static void main(String a[]) {
             try {
-                        //Montador e inicializador do input 
+                  // Montador e inicializador do input
                   ProcessBuilder montador = new ProcessBuilder(
-                        //linguagem
-                  "python", 
-                        //0 diretório
-                  System.getProperty("user.dir") + "\\Python\\Calculadora.py",
-                        //1... variáveis
-                  "28", "15");
+                              // linguagem
+                              "python",
+                              // 0 diretório
+                              System.getProperty("user.dir") + "\\Python\\Calculadora.py",
+                              // 1... variáveis
+                              "28", "15");
                   Process comando = montador.start();
 
-                        //Inicializa o leitor
+                  // Inicializa o leitor
                   BufferedReader leitorA = new BufferedReader(new InputStreamReader(comando.getInputStream()));
                   BufferedReader leitorB = new BufferedReader(new InputStreamReader(comando.getErrorStream()));
 
-                        //loop (enquanto tem informação mostra a linha)
+                  // loop (enquanto tem informação mostra a linha)
                   String linha = null;
                   while ((linha = leitorA.readLine()) != null) {
                         System.out.println("linha - " + linha);
                   }
-                        //loop (se der null no meio do código)
+                  // loop (se der null no meio do código)
                   while ((linha = leitorB.readLine()) != null) {
                         System.out.println("erro linha - " + linha);
                   }
 
-                  //caso dér erro fatal
+                  // caso dér erro fatal
             } catch (Exception e) {
                   System.out.println(e);
             }
       }
 }
-
-
-
-
--- Python/Calculadora.py
+++ Python/Calculadora.py
@@ -1,8 +1,7 @@
-import numbers
 import sys
 import os
 
-workindirectory=os.getcwd()
+diretório=os.getcwd()
 print('Lista de argumentos: ', str(sys.argv))
 print('total de argumentos: ', len(sys.argv), ' argumentos')
 
@@ -12,7 +11,7 @@ def addition():
     print("O argumento [2] é :", sys.argv[2])
     num1=sys.argv[1]
     num2=sys.argv[2]
-    sumof=int(num1)+int(num2)
-    print("A soma de {0} e {1} é {2}". format(num1, num2, sumof))
+    soma=int(num1)+int(num2)
+    print("A soma de {0} e {1} é {2}".format(num1, num2, soma))
 
 addition()
